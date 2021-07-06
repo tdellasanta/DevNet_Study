624dabad40ea280fee2fb4573de38e045183adde
tdellasanta <thiago.dellasanta@gmail.com>
2021-07-05 17:54:56 -0700

    Gerar Token DNA

---
+++ TokenDNA.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+
+payload={}
+headers = {
+  'Authorization': 'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
